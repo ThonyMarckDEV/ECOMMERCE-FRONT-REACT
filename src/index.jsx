8a20da5370ccfee906b49896175610c580b2cd26
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2024-12-15 18:30:25 -0500

    a

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
+//import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
+import ReactDOM from 'react-dom'; // Cambia esta línea
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Create the root
