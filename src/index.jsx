e0e00930158612edf49f465457ca3ca53bb15774
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2024-12-15 18:32:58 -0500

    yaaaa

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,23 @@
+// import React from 'react';
+// //import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
+// import ReactDOM from 'react-dom'; // Cambia esta línea
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root')); // Create the root
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import React from 'react';
-//import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client'
-import ReactDOM from 'react-dom'; // Cambia esta línea
+import ReactDOM from 'react-dom'; // Importación normal
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); // Create the root
-root.render(
+// Con React 17 usa render() en lugar de createRoot()
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
