1051cdc06e4c4ded9463c376999bf516b6893947
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2024-12-23 18:15:50 -0500

    new url back

--- src/js/urlHelper.js
+++ src/js/urlHelper.js
@@ -1,4 +1,4 @@
 // src/js/urlHelper.js
-const API_BASE_URL = 'https://cartridge-doing-adaptation-tape.trycloudflare.com'; // Cambia esta URL si es necesario
+const API_BASE_URL = 'https://dutch-plot-republicans-bahrain.trycloudflare.com'; // Cambia esta URL si es necesario
 
 export default API_BASE_URL;
