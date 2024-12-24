56f5e9e9425287195b353ad74ca567367f43473f
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2024-12-24 16:27:26 -0500

    new back

--- src/js/urlHelper.js
+++ src/js/urlHelper.js
@@ -1,4 +1,4 @@
 // src/js/urlHelper.js
-const API_BASE_URL = 'https://pacific-rubber-byte-purchase.trycloudflare.com'; // Cambia esta URL si es necesario
+const API_BASE_URL = 'https://grant-fl-invalid-language.trycloudflare.com'; // Cambia esta URL si es necesario
 
 export default API_BASE_URL;
