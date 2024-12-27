b82ee53ef24d838061b042cff4a5cc482d429806
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2024-12-27 14:57:19 -0500

    aaaadasd

--- src/js/urlHelper.js
+++ src/js/urlHelper.js
@@ -1,4 +1,4 @@
 // src/js/urlHelper.js
-const API_BASE_URL = 'https://road-php-boss-fixes.trycloudflare.com'; // Cambia esta URL si es necesario
+const API_BASE_URL = 'https://conclusions-realty-punishment-money.trycloudflare.com'; // Cambia esta URL si es necesario
 
 export default API_BASE_URL;
