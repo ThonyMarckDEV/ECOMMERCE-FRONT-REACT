3e58b471d7d0267a6781111b71d41d4ed37caf57
Anthony Marck Mendoza Sanchez <thonymarckyt08@gmail.com>
2025-05-21 01:15:38 -0500

    updated

--- src/js/urlHelper.js
+++ src/js/urlHelper.js
@@ -1,4 +1,3 @@
 // src/js/urlHelper.js
-const API_BASE_URL = 'https://ecommerceback.thonymarckdev.online'; // Cambia esta URL si es necesario
-//const API_BASE_URL = 'http://localhost:8000'; // Cambia esta URL si es necesario
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 export default API_BASE_URL
